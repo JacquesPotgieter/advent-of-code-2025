d9ca4fa68d281f482e531adab47416192f01334c
jjacpot@amazon.com <jjacpot@amazon.com>
2025-12-06 22:33:20 +0100

    Solve Day 03 Part 02

--- src/day_03/solver.py
+++ src/day_03/solver.py
@@ -2,43 +2,45 @@ def solve(battery_list, logging=False):
     total_joltage = 0
 
     for battery in battery_list:
-        joltage = find_highest_joltage(battery, logging)
+        joltage = find_highest_joltage(battery, 12, logging)
         total_joltage += joltage
     
     return total_joltage
 
 
-def find_highest_joltage(battery, logging=False):
+def find_highest_joltage(battery, battery_size, logging=False):
     battery_string = str(battery)
+    battery_total_length = len(battery_string)
+
+    battery_joltage = ""
 
     if logging:
         print(f"Testing {battery=}")
-    # First Pos will always be largest number, assuming second Pos is at the end
-    first_pos = find_largest_value_position(battery_string[0:-1], logging)
-    first_value = battery_string[first_pos]
-
-    if logging:
-        print(f"First Value= {first_value}")
-
-    # Second Pos will always be largest number after the first Post
-    remaining_string = battery_string[first_pos+1:]
-    second_pos = find_largest_value_position(remaining_string, logging)
-    second_value = remaining_string[second_pos]
-    if logging:
-        print(f"Second Value= {second_value}")
+        print(f"Looking for size - {battery_size}")
     
-    joltage = int(f"{first_value}{second_value}")
-    if logging:
-        print(f"Final joltage is {joltage}")
+    battery_start_pos = 0
 
-    return joltage
+    for batteries_remaining in range(battery_size, 0, -1):
+        # We need at least Count positions for remaining values
+        battery_end_post = battery_total_length - batteries_remaining
+
+        # Next highest battery is at Pos
+        found_position = find_largest_value_position(battery_string, battery_start_pos, battery_end_post)
+
+        # Update starting search to exclude latest found Pos
+        battery_start_pos = found_position + 1
+
+        # Battery Value
+        battery_joltage = f"{battery_joltage}{battery_string[found_position]}"
+    
+    return int(battery_joltage)
 
 
-def find_largest_value_position(list_string, logging=False):
+def find_largest_value_position(list_string, search_start_pos, search_end_pos, logging=False):
     largest = 0
     largest_pos = 0
 
-    for counter in range(len(list_string)):
+    for counter in range(search_start_pos, search_end_pos + 1):
         compare = int(list_string[counter])
 
         if largest < compare:
--- src/day_03/test.py
+++ src/day_03/test.py
@@ -1,22 +1,27 @@
-from solver import find_highest_joltage, find_largest_value_position
+from solver import find_highest_joltage_v2, find_largest_value_position
 
-def test_find_largest_value_position(list_string, expected):
-    print(f"* (Testing {list_string=}) with {expected=}")
-    result = find_largest_value_position(list_string)
-    assert result == expected, f"({list_string=}) {expected=}, but returned {result}"
+def test_find_largest_value_position(list_string, start, end, expected):
+    print(f"* (Testing {list_string=} between Pos ({start} & {end}) with {expected=}")
+    result = find_largest_value_position(list_string, start, end)
+    assert result == expected, f"({list_string=}) between Pos ({start} & {end}) {expected=}, but returned {result}"
 
-def test_find_highest_joltage(battery, expected, logging=False):
+def test_find_highest_joltage(battery, size, expected, logging=False):
     print(f"* (Testing {battery=}) with {expected=}")
-    result = find_highest_joltage(battery, logging)
+    result = find_highest_joltage_v2(battery, size, logging)
     assert result == expected, f"({battery=}) {expected=}, but returned {result}"
 
 print("find_largest_value_position --> ")
-test_find_largest_value_position("123", 2)
-test_find_largest_value_position("132", 1)
-test_find_largest_value_position("321", 0)
+test_find_largest_value_position("0123456", 0, 6, 6)
+test_find_largest_value_position("0123456", 0, 4, 4)
+test_find_largest_value_position("6543210", 0, 6, 0)
+test_find_largest_value_position("6543210", 2, 6, 2)
 
 
 print("\n find_highest_joltage --> ")
-test_find_highest_joltage(9876, 98)
-test_find_highest_joltage(1234, 34)
-test_find_highest_joltage(119811, 98)
+test_find_highest_joltage(9876, 2, 98)
+test_find_highest_joltage(1234, 2, 34)
+test_find_highest_joltage(119811, 2, 98)
+
+test_find_highest_joltage(9876, 3, 987)
+test_find_highest_joltage(1234, 3, 234)
+test_find_highest_joltage(1198119, 3, 989)
